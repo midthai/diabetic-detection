eff8b79deef0ce120521ef33d1fbc1f96efd1265
nitikorn <nitikornchumnankul@gmail.com>
2023-12-13 16:55:51 +0700

    prototype

--- diabetic-detection/src/App.vue
+++ diabetic-detection/src/App.vue
@@ -1,26 +1,131 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="upload-container">
+    <!-- Card layout for image upload -->
+    <div class="card">
+      <!-- Logo at the top of the card -->
+      <div class="logo-container">
+        <img src="../src/assets/Colaboration_logo.png" alt="Center Logo" class="logo" />
+      </div>
+      
+      <!-- Image preview -->
+      <div v-if="imagePreview" class="image-preview-container">
+        <img :src="imagePreview" alt="Image preview" class="image-preview" />
+      </div>
+
+      <!-- Upload input and button -->
+      <div class="upload-input">
+        <input type="file" @change="onFileChange">
+        <button @click="uploadImage">Upload Image</button>
+      </div>
+
+      <!-- Display the response data -->
+      <div v-if="uploadResponse" class="response">
+       <h1>Prediction:  {{ uploadResponse }}</h1>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const selectedFile = ref(null);
+const uploadResponse = ref('');
+const imagePreview = ref(null);
+
+const onFileChange = (e) => {
+  selectedFile.value = e.target.files[0];
+  imagePreview.value = URL.createObjectURL(selectedFile.value);
+};
+
+const uploadImage = async () => {
+  if (!selectedFile.value) {
+    alert('Please select a file first.');
+    return;
   }
-}
+
+  const formData = new FormData();
+  formData.append('file', selectedFile.value);
+
+  try {
+    const response = await axios.post('http://202.151.176.129:8000/uploadfile/', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    uploadResponse.value = response.data;
+    console.log(response.data)
+    URL.revokeObjectURL(imagePreview.value);
+
+    console.log('Server response:', response.data.predicted_label); // Log the server response
+  } catch (error) {
+    console.error('Error uploading image:', error);
+    uploadResponse.value = 'Error uploading image: ' + error.message;
+    URL.revokeObjectURL(imagePreview.value);
+  }
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.upload-container {
+  display: flex;
+  justify-content: center;
+  padding-top: 50px;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  border-radius: 5px;
+  width: 500px;
+  padding: 50px;
+  background: white;
+}
+
+.logo-container {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-bottom: 20px;
+}
+
+.logo {
+  max-width: 500px;
+  max-height: 500px;
+}
+
+.image-preview-container {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.image-preview {
+  max-width: 100%;
+  max-height: 400px;
+  border-radius: 5px;
+}
+
+.upload-input {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+button {
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  background-color: #008CBA;
+  color: white;
+  cursor: pointer;
+  font-size: 1em;
+}
+
+button:hover {
+  background-color: #005f73;
+}
+
+.response {
+  margin-top: 20px;
+  text-align: center;
+  font-weight: bold;
 }
 </style>
--- diabetic-detection/src/components/HelloWorld.vue
+++ diabetic-detection/src/components/HelloWorld.vue
@@ -1,58 +1,130 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="upload-container">
+    <!-- Card layout for image upload -->
+    <div class="card">
+      <!-- Logo at the top of the card -->
+      <div class="logo-container">
+        <img src="../assets/Colaboration_logo.png" alt="Center Logo" class="logo" />
+      </div>
+      
+      <!-- Image preview -->
+      <div v-if="imagePreview" class="image-preview-container">
+        <img :src="imagePreview" alt="Image preview" class="image-preview" />
+      </div>
+
+      <!-- Upload input and button -->
+      <div class="upload-input">
+        <input type="file" @change="onFileChange">
+        <button @click="uploadImage">Upload Image</button>
+      </div>
+
+      <!-- Display the response data -->
+      <div v-if="uploadResponse" class="response">
+       <p>Prediction:  {{ uploadResponse }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const selectedFile = ref(null);
+const uploadResponse = ref('');
+const imagePreview = ref(null);
+
+const onFileChange = (e) => {
+  selectedFile.value = e.target.files[0];
+  imagePreview.value = URL.createObjectURL(selectedFile.value);
+};
+
+const uploadImage = async () => {
+  if (!selectedFile.value) {
+    alert('Please select a file first.');
+    return;
   }
-}
+
+  const formData = new FormData();
+  formData.append('file', selectedFile.value);
+
+  try {
+    const response = await axios.post('http://202.151.176.129:8000/uploadfile/', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    uploadResponse.value = response.data.predicted_label;
+    URL.revokeObjectURL(imagePreview.value);
+
+    console.log('Server response:', response.data.predicted_label); // Log the server response
+  } catch (error) {
+    console.error('Error uploading image:', error);
+    uploadResponse.value = 'Error uploading image: ' + error.message;
+    URL.revokeObjectURL(imagePreview.value);
+  }
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.upload-container {
+  display: flex;
+  justify-content: center;
+  padding-top: 50px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  border-radius: 5px;
+  width: 350px;
+  padding: 20px;
+  background: white;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.logo-container {
+  text-align: center;
+  margin-bottom: 20px;
 }
-a {
-  color: #42b983;
+
+.logo {
+  max-width: 100px;
+  max-height: 100px;
+}
+
+.image-preview-container {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.image-preview {
+  max-width: 100%;
+  max-height: 200px;
+  border-radius: 5px;
+}
+
+.upload-input {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+button {
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  background-color: #008CBA;
+  color: white;
+  cursor: pointer;
+  font-size: 1em;
+}
+
+button:hover {
+  background-color: #005f73;
+}
+
+.response {
+  margin-top: 20px;
+  text-align: center;
+  font-weight: bold;
 }
 </style>
